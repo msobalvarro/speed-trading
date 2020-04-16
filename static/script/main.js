5d5066878c6be3a9d1a3f563e5cb93360ced562a
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-04-16 16:47:32 -0600

    get in on play store button

--- static/script/main.js
+++ static/script/main.js
@@ -1,22 +1,5 @@
 const $ = (selector = '') => document.querySelector(selector)
 
-$('#closer').addEventListener('click', () => {
-    $('#modalRegister').classList.toggle('hidden')
-})
-
-$('#open').addEventListener('click', () => {
-    $('#modalCondiciones').classList.toggle('hidden')
-})
-
-$('#openUpgrade').addEventListener('click', () => {
-    $('#modalUpgrade').classList.toggle('hidden')
-})
-
-$('#closerUpgrade').addEventListener('click', () => {
-    $('#modalUpgrade').classList.toggle('hidden')
-})
-
-
 $('#openCondiciones').addEventListener('click', () => {
     $('#modalCondiciones').classList.toggle('hidden')
 })
@@ -24,11 +7,5 @@ $('#openCondiciones').addEventListener('click', () => {
 $('#closeCondiciones').addEventListener('click', () => {
     $('#modalCondiciones').classList.toggle('hidden')
 })
-
-$('#openRegisterFromConditions').addEventListener('click', () => {
-    $('#modalRegister').classList.toggle('hidden')
-    $('#modalCondiciones').classList.toggle('hidden')
-})
-
 
 $('#year').innerHTML = new Date().getFullYear()
