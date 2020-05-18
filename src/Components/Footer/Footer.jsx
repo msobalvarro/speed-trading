72feb8b80324f0f7e2cedc6431ede4fffa6557f6
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-05-18 14:52:13 -0600

    adding facebook link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,15 +3,22 @@ import React from 'react'
 // Import image and assets
 import './Footer.scss'
 import AlySystem from '../../Assets/alysystem.png'
+import FacebookIcon from '../../Assets/facebook-icon.png'
 
 const Footer = () => {
     return (
         <footer>
             <img src={AlySystem} alt="alysystem" />
 
-            <span>
-                &copy; Copyright {new Date().getFullYear()}
-            </span>
+            <div className="block">
+                <a href="https://www.facebook.com/Speed-Tradings-106534004312000" target="_blank" className="facebook">
+                    <img src={FacebookIcon} alt="Facebook" />
+                </a>
+
+                <span>
+                    &copy; Copyright {new Date().getFullYear()}
+                </span>
+            </div>
 
             <a target="_blank" className="support" href="mailto:tradingspeed4@gmail.com?subject=Soporte">Enviar email a soporte</a>
         </footer>
