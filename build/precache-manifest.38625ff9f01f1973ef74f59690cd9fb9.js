3ae5537db95f41e2cacc7f4780801c61c545307d
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-05-18 15:17:44 -0600

    build site

---
+++ build/precache-manifest.38625ff9f01f1973ef74f59690cd9fb9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5433cc42f2ffaed281f2c714a2bbad9b",
+    "url": "/speed-trading/index.html"
+  },
+  {
+    "revision": "a9a528c1be694ac08135",
+    "url": "/speed-trading/static/css/main.7a37ae08.chunk.css"
+  },
+  {
+    "revision": "793df9fae9d94bb0e030",
+    "url": "/speed-trading/static/js/2.82d0789e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-trading/static/js/2.82d0789e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a528c1be694ac08135",
+    "url": "/speed-trading/static/js/main.9a4f21e9.chunk.js"
+  },
+  {
+    "revision": "c6ab341be5d812d544b0",
+    "url": "/speed-trading/static/js/runtime-main.28a39e3a.js"
+  },
+  {
+    "revision": "87df90d09b7b18b47eb655299eb7e3cd",
+    "url": "/speed-trading/static/media/alypay.87df90d0.png"
+  },
+  {
+    "revision": "4e779b6ccd0ef6d4d7ba7f6e21fd650b",
+    "url": "/speed-trading/static/media/banner-cover.4e779b6c.png"
+  },
+  {
+    "revision": "b2818639dece7a951de152ac29748b0d",
+    "url": "/speed-trading/static/media/custom-plan.b2818639.png"
+  },
+  {
+    "revision": "d68b84421cfc4661a2615a18f2ba5703",
+    "url": "/speed-trading/static/media/dashboard.d68b8442.png"
+  },
+  {
+    "revision": "a59915d3a129497606fb6b730bafbcad",
+    "url": "/speed-trading/static/media/exchange.a59915d3.png"
+  },
+  {
+    "revision": "a55e71aea268e8cde6d85a38b6ea8578",
+    "url": "/speed-trading/static/media/logo.a55e71ae.png"
+  },
+  {
+    "revision": "06bbc19ac2bca77e32bd27ff4f539423",
+    "url": "/speed-trading/static/media/ref.06bbc19a.png"
+  },
+  {
+    "revision": "bbd6a9429b1758bbc723a21a62fd04ec",
+    "url": "/speed-trading/static/media/splash.bbd6a942.png"
+  }
+]);
