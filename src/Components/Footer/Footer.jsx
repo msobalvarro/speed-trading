a91aeef5a1783baee884670b2837b8a04e8b7a75
Harold Espinoza <haroldesptru@gmail.com>
2020-12-24 11:15:01 -0600

    Añadidas direcciones de las oficinas+

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 // Import image and assets
 import './Footer.scss'
-import AlySystem from '../../Assets/alysystem.png'
 import AlyCoin from '../../Assets/AlyCoin.png'
 import AlyCoinLetters from '../../Assets/AlyCoin-letters.png'
+import { ReactComponent as LocationIcon } from '../../Assets/location-pin.svg'
 
 const urlPDF = "https://drive.google.com/file/d/13_2tfprMchj06zf5dfZVR-7VPkY0JGCj/view?usp=sharing"
 
@@ -17,11 +17,10 @@ const Footer = () => {
     const instagram = 'https://instagram.com/alysystem_1?igshid=1gbeiyxfj3i68'
     const telegram = 'https://t.me/joinchat/NUHb5RppOxJX9-EsU041mQ'
     const whatsapp = 'https://wa.me/+50660727720'
-    //const SpeedTradingsFacebook = 'https://www.facebook.com/Speed-Tradings-106534004312000'
 
     useEffect(_ => {
         setTimeout(_ => {
-            setActive((active===3) ? 1 : (active + 1))
+            setActive((active === 3) ? 1 : (active + 1))
         }, 5000)
     }, [active])
 
@@ -31,29 +30,38 @@ const Footer = () => {
                 <div className="Copyright">
                     <span>&copy;Copyright {new Date().getFullYear()} AlySystem | RUC J031000037155</span>
 
+                    <div className='locations'>
+                        <LocationIcon />
+                        Frente club terraza, Edificio Discover. Tercer piso. Managua, Nicaragua
+                    </div>
+                    <div className='locations'>
+                        <LocationIcon />
+                        Edificio Torrez, Paseo Colón. Piso No. 9, Oficina No.909. San José, Costa Rica
+                    </div>
+
                     <div className='social-links'>
-                        <a href={twitter} target='_blank' rel="noopener noreferrer" className="social-icon twitter"></a>
-                        <a href={facebook} target='_blank' rel="noopener noreferrer" className='social-icon facebook'></a>
-                        <a href={instagram} target='_blank' rel="noopener noreferrer" className='social-icon instagram'></a>
-                        <a href={telegram} target='_blank' rel="noopener noreferrer" className='social-icon telegram'></a>
-                        <a href={whatsapp} target='_blank' rel="noopener noreferrer" className='social-icon whatsapp'></a>
+                        <a href={twitter} target='_blank' rel={"noopener noreferrer"} className="social-icon twitter"></a>
+                        <a href={facebook} target='_blank' rel={"noopener noreferrer"} className='social-icon facebook'></a>
+                        <a href={instagram} target='_blank' rel={"noopener noreferrer"} className='social-icon instagram'></a>
+                        <a href={telegram} target='_blank' rel={"noopener noreferrer"} className='social-icon telegram'></a>
+                        <a href={whatsapp} target='_blank' rel={"noopener noreferrer"} className='social-icon whatsapp'></a>
                     </div>
                 </div>
 
                 <div className='support-container'>
-                    <p className={active===1 ? 'active' : ''}>
-                        ¿Consultas? <a href={urlPDF} target="_blank" download>ver más información</a>
+                    <p className={active === 1 ? 'active' : ''}>
+                        ¿Consultas? <a href={urlPDF} target="_blank" rel={"noopener noreferrer"} download>ver más información</a>
                     </p>
 
-                    <p className={active===2 ? 'active' : ''}>
-                        ¿Dudas? <a href={urlPDF} target="_blank" download>ver más información</a>
+                    <p className={active === 2 ? 'active' : ''}>
+                        ¿Dudas? <a href={urlPDF} target="_blank" rel={"noopener noreferrer"} download>ver más información</a>
                     </p>
 
-                    <p className={active===3 ? 'active' : ''}>
-                        ¿Inquietudes? <a href={urlPDF} target="_blank" download>ver más información</a>
+                    <p className={active === 3 ? 'active' : ''}>
+                        ¿Inquietudes? <a href={urlPDF} target="_blank" rel={"noopener noreferrer"} download>ver más información</a>
                     </p>
 
-                    <a target="_blank" className="support" href="mailto:tradingspeed4@gmail.com?subject=Soporte">
+                    <a target="_blank" rel={"noopener noreferrer"} className="support" href="mailto:soporte@alysystem.com?subject=Soporte AlySystem">
                         <span className='social-icon email'></span>
                         Contactar a soporte
                     </a>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, } from 'react'
+import React, { useState } from 'react'
 
 // Import styles and assets
 import './Navbar.scss'
@@ -11,13 +11,13 @@ const Navbar = () => {
     const [showNavbar, setShowNavbar] = useState(false)
 
     const onTopScroll = _ => {
-        document.querySelector('body').scrollTo({top: 0, behavior: 'smooth'})
+        document.querySelector('body').scrollTo({ top: 0, behavior: 'smooth' })
     }
 
     return (
         <nav className="navigaion-bar">
-            <img src={Logo} className="brand-logo" alt="logo"/>
-            <img src={Menu} onClick={_ => setShowNavbar(true)} className="menu" alt="menu"/>
+            <img src={Logo} className="brand-logo" alt="logo" />
+            <img src={Menu} onClick={_ => setShowNavbar(true)} className="menu" alt="menu" />
 
             <div className={`list ${showNavbar ? 'active' : ''}`}>
                 <a onClick={_ => setShowNavbar(false)} href="#exchange" className="item">AlyExchange</a>
@@ -29,13 +29,13 @@ const Navbar = () => {
                 <a onClick={_ => setShowNavbar(false)} href="https://www.speedtradings-bank.com" target="_bank" className="item">Dashboard</a>
 
                 <a onClick={_ => setShowNavbar(false)} href="#plans" className="item">Planes de inversión</a>
-                
+
                 <a onClick={_ => setShowNavbar(false)} href="#register" className="item">Registrarme</a>
 
-                <img src={Close} onClick={_ => setShowNavbar(false)} className="close" alt="close"/>
+                <img src={Close} onClick={_ => setShowNavbar(false)} className="close" alt="close" />
             </div>
 
-            <img onClick={_ => onTopScroll()} src={ArrowUp} alt="" title="Ir al inicio" className="top-scroll"/>
+            <img onClick={_ => onTopScroll()} src={ArrowUp} alt="" title="Ir al inicio" className="top-scroll" />
         </nav>
     )
 }
