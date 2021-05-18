2efba3df47f2fb12e7308c47422addc0dd6667ea
msobalvarro <mssrsobalvarro1997@gmail.com>
2021-05-18 15:36:21 -0600

    Updates

---
+++ precache-manifest.2ba5db58abefa4206142d459f3877886.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e68b4143a72cc7ddd30229501ae01e1",
+    "url": "/speed-trading/index.html"
+  },
+  {
+    "revision": "2183de2853e35b060e6b",
+    "url": "/speed-trading/static/css/main.3c15dabd.chunk.css"
+  },
+  {
+    "revision": "e9a10951f259145e3b50",
+    "url": "/speed-trading/static/js/2.ce2f6476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-trading/static/js/2.ce2f6476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2183de2853e35b060e6b",
+    "url": "/speed-trading/static/js/main.211cbb5a.chunk.js"
+  },
+  {
+    "revision": "6d050df3fa37e1b9b117",
+    "url": "/speed-trading/static/js/runtime-main.72ac9a50.js"
+  },
+  {
+    "revision": "90871585ae52c4a84d9590c5c9f072a1",
+    "url": "/speed-trading/static/media/AlyCoin-letters.90871585.png"
+  },
+  {
+    "revision": "e5419a739754a17d87d5b484aa893967",
+    "url": "/speed-trading/static/media/AlyCoin.e5419a73.png"
+  },
+  {
+    "revision": "8d742d35eecdf612446b067218220a86",
+    "url": "/speed-trading/static/media/alypay1.8d742d35.jpeg"
+  },
+  {
+    "revision": "dcaa10fcf9f14e3c50a44e2909d16f9f",
+    "url": "/speed-trading/static/media/alypay2.dcaa10fc.jpeg"
+  },
+  {
+    "revision": "86a98423f2276749d97af0cc72d4109d",
+    "url": "/speed-trading/static/media/alypay3.86a98423.jpeg"
+  },
+  {
+    "revision": "8278977383f9ec53de99c0362887d36f",
+    "url": "/speed-trading/static/media/alypay4.82789773.jpeg"
+  },
+  {
+    "revision": "9d5b17d16e2b74e0319c9ac0e8893176",
+    "url": "/speed-trading/static/media/alypay5.9d5b17d1.jpeg"
+  },
+  {
+    "revision": "414418da5c1d1591a2b6956d8f919479",
+    "url": "/speed-trading/static/media/alysystem.414418da.png"
+  },
+  {
+    "revision": "988b096efd49038873110aa18df5086a",
+    "url": "/speed-trading/static/media/android.988b096e.svg"
+  },
+  {
+    "revision": "20857cc54ff350c0867a054613411bc7",
+    "url": "/speed-trading/static/media/apple.20857cc5.svg"
+  },
+  {
+    "revision": "306b62a9f58d2a91668a55ddc387ebee",
+    "url": "/speed-trading/static/media/arrow-up.306b62a9.svg"
+  },
+  {
+    "revision": "c06bbb3167583777720e5d8d8a8f3675",
+    "url": "/speed-trading/static/media/basic-info.c06bbb31.png"
+  },
+  {
+    "revision": "38c34a1b03d3695d22e87c9d1af27731",
+    "url": "/speed-trading/static/media/bitcoin.38c34a1b.svg"
+  },
+  {
+    "revision": "adae651ddbf19d1b845466ce859640a1",
+    "url": "/speed-trading/static/media/close.adae651d.svg"
+  },
+  {
+    "revision": "579accca15ba0f01c0b994a8c385906d",
+    "url": "/speed-trading/static/media/custom-plan.579accca.png"
+  },
+  {
+    "revision": "70a8cd9733023db8d56e774e0b5c44a8",
+    "url": "/speed-trading/static/media/dashboard-app.70a8cd97.png"
+  },
+  {
+    "revision": "802c6eacbb13c83b27c39be505228231",
+    "url": "/speed-trading/static/media/ethereum.802c6eac.svg"
+  },
+  {
+    "revision": "c41cc49221153a1e8b14aaf8a7ec0d22",
+    "url": "/speed-trading/static/media/exchange.c41cc492.jpeg"
+  },
+  {
+    "revision": "7c1b8f2b15b1acad707a9a0c9638bfc3",
+    "url": "/speed-trading/static/media/exchange2.7c1b8f2b.jpeg"
+  },
+  {
+    "revision": "114ec3e93f048a5a39a449db206d7896",
+    "url": "/speed-trading/static/media/exchange3.114ec3e9.jpeg"
+  },
+  {
+    "revision": "c0a81ee4c386e61c42b1e0e7f55ea8ba",
+    "url": "/speed-trading/static/media/exchange4.c0a81ee4.jpeg"
+  },
+  {
+    "revision": "e4f607e6dae12c63f1a1210fc806d052",
+    "url": "/speed-trading/static/media/location-pin.e4f607e6.svg"
+  },
+  {
+    "revision": "6898ed30bcbcc310b9b4985a177eecf2",
+    "url": "/speed-trading/static/media/login-app.6898ed30.png"
+  },
+  {
+    "revision": "361b738515b8627dc70ad8cd2234182c",
+    "url": "/speed-trading/static/media/menu.361b7385.svg"
+  },
+  {
+    "revision": "4524696dd32407657869f2c338b40335",
+    "url": "/speed-trading/static/media/ref.4524696d.png"
+  },
+  {
+    "revision": "a3cf47140370f8ea37a2650e8cf2fb6f",
+    "url": "/speed-trading/static/media/speed-banner-1.a3cf4714.png"
+  },
+  {
+    "revision": "03ebff889bdec3387289404f3cbaa9e0",
+    "url": "/speed-trading/static/media/speed-banner-2.03ebff88.png"
+  },
+  {
+    "revision": "f5a83376b4f551bc3c4d1ecdae115aaa",
+    "url": "/speed-trading/static/media/splash.f5a83376.png"
+  },
+  {
+    "revision": "9571c61f68fa8036e89eadafd4af815d",
+    "url": "/speed-trading/static/media/telephone.9571c61f.png"
+  }
+]);
